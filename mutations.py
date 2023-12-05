0e5f82d9182ef2a5c264bd248fc2c147e5191c68
Keith Mathe <keithmathe2002@gmail.com>
2023-12-05 14:32:03 -0500

    Change error check to at least l <= 1

--- mutations.py
+++ mutations.py
@@ -5,7 +5,7 @@ from itertools import permutations
 def mutationA(seed: str)->str:
     """Remove a random byte"""
     l = len(seed)
-    if (l <= 0):
+    if (l <= 1):
         return choice(string.ascii_letters)
     r = randint(0, l - 1)
     return seed[:r] + seed[r+1:]
@@ -23,14 +23,14 @@ def mutationB(seed: str)->str:
 def mutationC(seed: str)->str:
     """Insert a random byte"""
     l = len(seed)
-    if (l <= 0):
-        return choice(string.ascii_letters)
+    if (l <= 1):
+        return seed + choice(string.ascii_letters)
     r = randint(0, l - 1)
     return seed[:r] + choice(string.ascii_letters + string.digits) + seed[r:]
 
 def mutationD(seed: str)->str:
     """Replace bytes with random values"""
-    if (len(seed) <= 0):
+    if (len(seed) <= 1):
         seed = choice(string.ascii_letters)
     for i in range(randint(0, len(seed))):
         seed = mutationC(mutationA(seed))
@@ -38,15 +38,15 @@ def mutationD(seed: str)->str:
 
 def mutationE(seed: str)->str:
     """Return a random length slice of the string from index 0"""
-    if (len(seed) <= 0):
-        return choice(string.ascii_letters)
+    if (len(seed) <= 1):
+        return choice(string.ascii_letters) + seed
     r = randint(0, len(seed) - 1)
     return seed[:r]
 
 def mutationF(seed: str)->str:
     """Return a random length slice of the string from any index"""
-    if (len(seed) <= 0):
-        return choice(string.digits)
+    if (len(seed) <= 1):
+        return seed + choice(string.digits)
     r = randint(0, len(seed) - 1)
     return seed[r:]
 
@@ -56,15 +56,15 @@ def mutationG(seed: str)->str:
 
 def mutationH(seed: str)->str:
     """Concatanate a string with its slice from index 0"""
-    if (len(seed) <= 0):
-        return choice(string.ascii_letters)
+    if (len(seed) <= 1):
+        return seed + choice(string.ascii_letters)
     r = randint(0, len(seed) - 1)
     return seed + seed[r:]  
 
 def mutationI(seed: str)->str:
     """Replace a random character in a string with a random character"""
     l = choice(string.digits + string.ascii_letters)
-    if (len(seed) <= 0):
+    if (len(seed) <= 1):
         return choice(string.ascii_letters)
     r = randint(0, len(seed) - 1)
     return seed.replace(seed[r], l)
