92a4cc106d843e72b93f38c5f10f0832f62300a5
Rafael Sakamoto <kenzo@sakamoto.com.br>
2023-12-04 20:22:59 -0500

    removed prints

--- fuzzer.py
+++ fuzzer.py
@@ -51,23 +51,16 @@ def run_command(file, input):
 
         # Run test with branch coverage, and write coverage data to data_file
         result = subprocess.run([f'coverage run --branch --data-file "{data_file}" "{file}" "{input}"'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        print(result.stdout)
-        print(result.stderr)
+
         # Get report in json format and write it to report_file
         cov = subprocess.run([f'coverage json --pretty --data-file "{data_file}" -o "{report_file}"'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        print(cov.stdout)
-        print(cov.stderr)
+
         # Call mutation function
-        print("[before]report_file:", report_file)
-        print("[before]data_file:", data_file)
         with open(report_file) as report:
-            print("report_file:", report_file)
-            print("report:", report)
             coverage_data = json.load(report)
             mutator(result.returncode, coverage_data, input)
-            available_files.put(file)
-            print("available_files:", available_files)
-        print("result:", result)
+
+        available_files.put(file)
         return result
     except subprocess.CalledProcessError as e:
         print(f"Error executing command '{file}':\n{e.stderr}")
@@ -117,14 +110,14 @@ def main():
         while time.time() - START_TIME < args.timeout:
             # Gets next available file or blocks until a new one is enqueued
             cur_file = available_files.get(block=True)
-            print("cur_file:", cur_file)
 
             # Get next input
             next_input = get_next_input()
 
             # Mutate input
+            print('Mutating..')
             next_input = mutations.select_mutation_function()(next_input)
-            print("next_input:", next_input)
+            print('Done mutating..')
 
             # Submit each command to the ThreadPoolExecutor
             if platform.system() == 'Windows':
